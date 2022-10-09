ffde98e5eceaf767aa16ae66386a9dbcf6986668
Astika Luintel <astild2020@gmail.com>
2022-10-08 20:23:12 -0400

    commit tree 35472a88c62fa1d3948636f20dcbcad100a6942f

--- methods.rb
+++ methods.rb
@@ -1,1 +1,35 @@
 # Your code here!
+def greet_programmer ()
+   puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+ greet "Naureen"
+ greet "Jimmy"
+
+ def greet_with_default (name = "programmer")
+    puts "Hello, #{name}!"
+ end
+
+ greet_with_default
+
+ def add(num1, num2)
+     num1 + num2
+ end
+
+add(2, 5)
+
+def halve(number)
+    if number == number
+        number / 2
+    else 
+        return nil
+    end
+
+end
+halve(6)
+
+
+
